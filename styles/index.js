aabeb393f293bf6dd264fc1d9d4a6b688d5b06a7
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-05-14 18:56:29 +0300

    first commit

---
+++ styles/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("click", function () {
+    let request = document.getElementById('request').value;
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=CZydw9PcANkq5vN3W3LF0Ee2PEfEi6hg&&limit=5&&q=' + request)
+        .then(response => response.json())
+        .then(gifs => {
+            let items = gifs;
+            let gifsContent = "";
+            for (let i = 0; i < 5; i++) {
+                gifsContent += `<div class="gifs">
+            <img src="${items.data[i].images.original.url}>"
+            </div>`
+            }
+            document.querySelector('.pictures').innerHTML = gifsContent;
+            console.log(items, 'item');
+        })
+
+})
